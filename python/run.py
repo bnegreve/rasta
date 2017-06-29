6985210b170c873ed4d42038e047cd8ae0f6470e
alecoutre <adrian.lecoutre@gmail.com>
2017-06-29 13:52:04 +0200

    update run and requirements

--- python/run.py
+++ python/run.py
@@ -1,51 +1,50 @@
-from keras.layers.convolutional import Conv2D
-from models.alexnet import Alexnet,decaf
+from models.alexnet import decaf
 from models.processing import train_model_from_directory
 from keras.layers import Dense
-from keras.models import Model,Sequential
+from keras.models import Model
 from keras.layers.pooling import GlobalAveragePooling2D
 from keras.applications.resnet50 import ResNet50
 from keras import backend as K
 import os
-import sys
 from os.path import join
-
-
+import argparse
 
 PATH = os.path.dirname(__file__)
 
-vals = sys.argv
 
-id = vals[1]
-batch_size = int(vals[2]) 
-epochs = int(vals[3])
-steps = int(vals[4])
-val_steps = int(vals[5])
-flip = (vals[6]=='True')
+parser = argparse.ArgumentParser(description='Description')
+
+parser.add_argument('-m', action="store", default='decaf6',dest='model_name',help='Name of the model [decaf|decaf6|decaf7|resnet|resnetfull|resnetfullempty]')
+parser.add_argument('-b', action="store", default=32, type=int,dest='batch_size',help='Size of the batch.')
+parser.add_argument('-e', action="store",default=100,type=int,dest='epochs',help='Number of epochs')
+parser.add_argument('-f', action="store", default=False, type=bool,dest='horizontal_flip',help='Set horizontal flip or not [True|False]')
+parser.add_argument('--train_path', action="store", default=join(PATH, '../data/wikipaintings_train'),dest='training_path',help='Path of the training data directory')
+parser.add_argument('--val_path', action="store", default=join(PATH, '../data/wikipaintings_val'),dest='validation_path',help='Path of the validation data directory')
 
 
+args = parser.parse_args()
 
-    
-
+model_name = args.model_name
+batch_size = args.batch_size
+epochs = args.epochs
+flip = args.horizontal_flip
+TRAINING_PATH = args.training_path
+VAL_PATH = args.validation_path
 
 K.set_image_data_format('channels_first')
 size = (227,227)
-TRAINING_PATH = join(PATH, '../data/wikipaintings_train')
-VAL_PATH = join(PATH, '../data/wikipaintings_val')
-WEIGHTS_PATH = join(PATH,'models/weights/alexnet_weights.h5')
 
-if len(vals) > 8:
-    TRAINING_PATH = vals[7]
-    VAL_PATH = vals[8]
-    
-if id=='decaf5':
+WEIGHTS_PATH = join(PATH, 'models/weights/alexnet_weights.h5')
+
+if model_name =='decaf5':
+    WEIGHTS_PATH = join(PATH,'models/weights/alexnet_weights.h5')
     base_model = decaf(WEIGHTS_PATH,5)
     predictions = Dense(25, activation='softmax')(base_model.output)
     model = Model(inputs=base_model.input, outputs=predictions)
     for layer in base_model.layers:
         layer.trainable = False
 
-elif id=='decaf6':
+elif model_name =='decaf6':
     base_model = decaf(WEIGHTS_PATH,6)
     predictions = Dense(25, activation='softmax')(base_model.output)
     model = Model(inputs=base_model.input, outputs=predictions)
@@ -53,14 +52,14 @@ elif id=='decaf6':
         layer.trainable = False
 
 
-elif id=='decaf7':
+elif model_name =='decaf7':
     base_model = decaf(WEIGHTS_PATH,7)
     predictions = Dense(25, activation='softmax')(base_model.output)
     model = Model(inputs=base_model.input, outputs=predictions)
     for layer in base_model.layers:
         layer.trainable = False
 
-elif id=='resnet':
+elif model_name =='resnet':
     K.set_image_data_format('channels_last')
     base_model = ResNet50(include_top=False)
     x = GlobalAveragePooling2D()(base_model.output)
@@ -70,12 +69,29 @@ elif id=='resnet':
         layer.trainable = False
     size = (224,224)
 
-elif id == 'other':
-    model = Sequential()
-    model.add(Conv2D(128,(3,3)))
-    model.add(Dense(1000, input_shape=(227,227), activation='relu', name='hidden_layer_1'))
-    model.add(Dense(50, activation='relu', name='hidden_layer_2'))
-    model.add(Dense(25, activation='softmax', name='predictions'))
+elif model_name == 'resnetfull':
+    K.set_image_data_format('channels_last')
+    base_model = ResNet50(include_top=False)
+    x = GlobalAveragePooling2D()(base_model.output)
+    predictions = Dense(25, activation='softmax')(x)
+    model = Model(inputs=base_model.input, outputs=predictions)
+    list_trainable = ['dense_1','bn5c_branch2c','res5c_branch2c','bn5c_branch2b','res5c_branch2b','bn5c_branch2a','res5c_branch2a']
+    for layer in base_model.layers:
+        if layer.name in list_trainable:
+            layer.trainable = True
+        else:
+            layer.trainable = False
+    size = (224,224)
+
+elif model_name == 'resnetfullempty':
+    K.set_image_data_format('channels_last')
+    base_model = ResNet50(include_top=False)
+    x = GlobalAveragePooling2D()(base_model.output)
+    predictions = Dense(25, activation='softmax')(x)
+    model = Model(inputs=base_model.input, outputs=predictions)
+    for layer in base_model.layers:
+        layer.trainable = True
+    size = (224,224)
 
 model.compile(loss='categorical_crossentropy',optimizer='rmsprop',metrics=['accuracy'])
-train_model_from_directory(TRAINING_PATH,model,model_name=id,target_size=size,validation_path=VAL_PATH,epochs = epochs,batch_size = batch_size,steps_per_epoch=steps,validation_steps=val_steps,horizontal_flip=flip)
+train_model_from_directory(TRAINING_PATH,model,model_name=model_name,target_size=size,validation_path=VAL_PATH,epochs = epochs,batch_size = batch_size,horizontal_flip=flip)
